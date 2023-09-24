2e3979b517238f838edd3776ffb0a36ebc4a4e66
peanutbuttercandy <coolnutting123@gmail.com>
2023-09-24 14:24:55 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,42 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable object-curly-newline */
+/* eslint-disable linebreak-style */
+/* eslint-disable quotes */
+/* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
+/* eslint-disable linebreak-style */
+/* eslint-disable spaced-comment */
+/* eslint-disable indent */
+
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNotebyIdHandler, deleteNoteByIdHandler } = require("./handler");
+
+/* eslint-disable linebreak-style */
+const routes = [
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: addNoteHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: getNoteByIdHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: editNotebyIdHandler,
+    },
+    {
+        method: 'DEETE',
+        path: '/notes/{id}',
+        handler: deleteNoteByIdHandler,
+    },
+];
+
+module.exports = routes;
